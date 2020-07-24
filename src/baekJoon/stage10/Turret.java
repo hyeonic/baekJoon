9b3cc3a28fe4d3f84924d6aef69f072f1ce24b35
hyeonic <evan3566@gmail.com>
2020-07-24 22:53:50 +0900

    sort & brute force start!

--- src/baekJoon/stage10/Turret.java
+++ src/baekJoon/stage10/Turret.java
@@ -33,23 +33,38 @@ public class Turret {
 			int y2 = Integer.parseInt( st.nextToken() );
 			int r2 = Integer.parseInt( st.nextToken() );
 
-//			int d = (int) (Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) );
+			int d = (int) (Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) );
 
-//			if( d == Math.pow(r1 + r2, 2) ) { // 외접
-//				bw.write( 1 + "\n" );
+
+//			if(x1 == x2 && y1 == y2 && r1 == r2) { // 외접
+//				bw.write( -1 + "\n" );
 //			}else if( d == Math.pow(r2 - r1, 2) ) { // 내접
 //				bw.write( 1 + "\n" );
 //			}else if( d < Math.pow(r2 - r1, 2) ) { // 원 안에서 교차점이 없을 때
 //				bw.write( 0 + "\n" );
 //			}else if( d > Math.pow(r1 + r2, 2) ) { // 원 밖에서 교차점이 없을 때
 //				bw.write( 0 + "\n" );
-//			}else if( x1 == x2 && y1 == y2 && r1 == r2 ){
-//				bw.write( -1 + "\n" );
+//			}else if(d == Math.pow(r1 + r2, 2)  ){
+//				bw.write( 1 + "\n" );
 //			}else {
 //				bw.write( 2 + "\n" );
 //			}
+			if( d == Math.pow(r1 + r2, 2) ) { // 외접
+				bw.write( String.valueOf(1) );
+			}else if( d == Math.pow(r2 - r1, 2) ) { // 내접
+				bw.write( String.valueOf(1) );
+			}else if( d < Math.pow(r2 - r1, 2) ) { // 원 안에서 교차점이 없을 때
+				bw.write( String.valueOf(0) );
+			}else if( d > Math.pow(r1 + r2, 2) ) { // 원 밖에서 교차점이 없을 때
+				bw.write( String.valueOf(0) );
+			}else if( x1 == x2 && y1 == y2 && r1 == r2 ){
+				bw.write( String.valueOf(-1) );
+			}else {
+				bw.write( String.valueOf(2) );
+			}
 
-			bw.write( tangentPoint(x1, y1, r1, x2, y2, r2) + "\n" );
+			//bw.write( tangentPoint(x1, y1, r1, x2, y2, r2) + "\n" );
+
 		}
 
 		bw.flush();
