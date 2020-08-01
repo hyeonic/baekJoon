d3431bdd03d9dadb5a2e9ce42bf01878f6c4bbe3
hyeonic <evan3566@gmail.com>
2020-08-01 23:05:04 +0900

    backtracking 1 ~ 4 clear!

---
+++ src/baekJoon/stage15/NandM1.java
@@ -0,0 +1,63 @@
+package baekJoon.stage15;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// 15-1 15649 N과 M (1)
+// 순열
+// 순서를 생각하고 중복을 허락하지 않는 경우 이는 순열  nPr 이다.
+public class NandM1 {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws IOException {
+
+		StringTokenizer st = new StringTokenizer( br.readLine(), " " );
+		int n = Integer.parseInt( st.nextToken() ); // n개 중에
+		int m = Integer.parseInt( st.nextToken() ); // m개를 뽑는 것
+
+		int[] array = new int[n];
+		boolean[] visited = new boolean[n];
+
+		for( int i = 0; i < n; ++i ) {
+			array[i] = i + 1;
+		}
+
+		int[] output = new int[n];
+
+		permutation( array, output, visited, 0, n, m );
+
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+
+	static void permutation( int[] array, int[] output, boolean[] visited, int depth, int n, int m ) throws IOException {
+		if( depth == m ) {
+			print( output, visited, m );
+			return;
+		}
+
+		for( int i = 0; i < n; ++i ) {
+			if( visited[i] != true ) {
+				visited[i] = true;
+				output[depth] = array[i];
+				permutation( array, output, visited, depth + 1, n, m );
+				visited[i] = false;
+			}
+		}
+	}
+
+	static void print( int[] output, boolean[] visited, int n ) throws IOException {
+		for( int i = 0; i < n; ++i ) {
+			bw.write( output[i] + " " );
+
+		}
+		bw.write("\n");
+	}
+}
---
+++ src/baekJoon/test/Dp.java
@@ -0,0 +1,61 @@
+package baekJoon.test;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Dp {
+
+	private static int[][] a;
+	private static int[][] dp;
+
+
+	 public static void main(String[] args) throws IOException {
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	        int n = Integer.parseInt(br.readLine());
+	        a = new int[n][2];
+	        dp = new int[n][n];
+
+	        for (int i = 0; i < n; i++) {
+	            StringTokenizer st = new StringTokenizer(br.readLine());
+	            a[i][0] = Integer.parseInt(st.nextToken());
+	            a[i][1] = Integer.parseInt(st.nextToken());
+	        }
+
+
+//	        for (int i = 1; i < n; i++) {
+//	            for (int j = 0; j < n - i; j++) {
+//	                dp[j][j + i] = Integer.MAX_VALUE;
+//	                for (int k = 0; k < i; k++) {
+//	                    int cost = dp[j][j + k] + dp[j + k + 1][j + i] + a[j][0] * a[j + k][1] * a[j + i][1];
+////	                    System.out.println("cost : dp["+j+"]["+(j+k)+"] + dp["+(j+k+1)+"]["+(j+i)+"] + a["+j+"][0] * a["+(j+k)+"][1] * a["+(j+i)+"][1]");
+////	                    System.out.println("= "+dp[j][j + k]+" + "+ dp[j + k + 1][j + i] +" + "+ a[j][0] +" * "+ a[j + k][1] +" * "+ a[j + i][1]);
+////	                    System.out.println(i+" "+j+" "+k);
+////	                    System.out.println(cost+" "+dp[j][j+1]);
+//	                    dp[j][j + i] = Math.min(dp[j][j + i], cost);
+//	                }
+//	            }
+//	        }
+
+
+
+	        bw.write(dp[0][n - 1] + "\n");
+
+	        bw.flush();
+	        br.close();
+	        bw.close();
+	    }
+
+	 public static int[][] matrixChain( int n ) {
+
+		 int j;
+
+		 return null;
+	 }
+
+}
