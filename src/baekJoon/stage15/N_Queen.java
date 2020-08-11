e5ac6943931a250e308659d00856a4338e15ae23
hyeonic <evan3566@gmail.com>
2020-08-11 19:53:32 +0900

    dp 7, 8 clear

--- src/baekJoon/stage15/N_Queen.java
+++ src/baekJoon/stage15/N_Queen.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-// 10-5 9663 N-Queen
+// 15-5 9663 N-Queen
 public class N_Queen {
 
 	static int n;
